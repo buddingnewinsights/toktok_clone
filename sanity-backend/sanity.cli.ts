e3e715f89ad48bc2ac7b266fa70811cdd343a7d0
Buu Gia Huynh <buub1309120@gmail.com>
2023-03-05 00:20:15 +0700

    modify define cli config backend

--- sanity-backend/sanity.cli.ts
+++ sanity-backend/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'gv69ml1p',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
