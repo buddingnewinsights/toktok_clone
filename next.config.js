432ce299484b508ec18bf4fbaa5f967dbc6e6531
Buu Gia Huynh <buub1309120@gmail.com>
2023-03-05 11:33:22 +0700

    new

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
