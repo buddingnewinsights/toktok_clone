121bee10d5f28b473722ee7d47004b15e7d8537b
Buu Gia Huynh <buub1309120@gmail.com>
2023-03-05 11:35:51 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -3,20 +3,10 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
   },
-  
+
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com'],
-  },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty',
-      };
-    }
-
-    return config;
   },
 };
 
