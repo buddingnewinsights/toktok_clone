afbe24bb55a4cab829a0f0c7453c75c5225ec746
Buu Gia Huynh <buub1309120@gmail.com>
2023-03-05 10:53:08 +0700

    sanity/cli

--- sanity-backend/sanity.cli.ts
+++ sanity-backend/sanity.cli.ts
@@ -1,13 +1,8 @@
-import { defineCliConfig } from '@sanity/cli'
+import {defineCliConfig}  from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 'gv69ml1p',
     dataset: 'production',
   },
-  server: {
-    hostname: "localhost",
-    port: 3333,
-  },
-  vite: (config) => config,
 })
